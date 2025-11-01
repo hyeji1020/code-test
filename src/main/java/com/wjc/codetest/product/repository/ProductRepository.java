cae19910fde98e052a314be4a62bc96670b417d8
hyeji <hyeji6742@naver.com>
2025-11-01 21:15:22 +0900

    review: Repository 리뷰 작성

--- src/main/java/com/wjc/codetest/product/repository/ProductRepository.java
+++ src/main/java/com/wjc/codetest/product/repository/ProductRepository.java
@@ -13,6 +13,11 @@ import java.util.List;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+    /*
+    * 문제: Service 계층까지 category 값을 전달했지만, Repository 메서드에서 파라미터명을 name으로 잘못 선언하여 의도와 다르게 동작.
+    * 원인: String category 대신 String name으로 전달.
+    * 개선안: findAllByCategory(String category, Pageable pageable);
+    * */
     Page<Product> findAllByCategory(String name, Pageable pageable);
 
     @Query("SELECT DISTINCT p.category FROM Product p")
