3e918f0534ce97494ffd696de4fb2f8791a9e5ee
hyeji <hyeji6742@naver.com>
2025-11-02 19:36:51 +0900

    review: 리뷰 표현 수정

--- src/main/java/com/wjc/codetest/product/model/response/ProductListResponse.java
+++ src/main/java/com/wjc/codetest/product/model/response/ProductListResponse.java
@@ -17,12 +17,10 @@ import java.util.List;
 /*
  * 공통문제 및 원인:
  * 1. @Setter 사용 - 응답 DTO는 불변이어야 하지만 setter로 수정 가능.
- * 2. 기본 생성자 누락 - JSON 역직렬화시 에러 발생.
  *
  * 개선안:
  * 1. @Setter 삭제
- * 2. @NoArgsConstructor 추가
- * 3. Record를 사용하여 1, 2번 개선사항을 자동으로 처리하여 더욱 간결하게 수정.
+ * 2. Record를 사용하여 1의 개선사항을 자동으로 처리하여 더욱 간결하게 수정.
  */
 @Getter
 @Setter
@@ -31,7 +29,7 @@ public class ProductListResponse {
     * 문제 및 원인: Entity 반환
     * 1. DB 유출 가능성.
     * 2. 연관 관계 포함 시 N+1 문제 발생 가능성.
-    * 개선안: ProductResponse를 생성하여 필요한 필드만 반환.
+    * 개선안: ProductResponse를 생성하여 직접적인 Entity 사용 X.
     * */
     private List<Product> products;
     private int totalPages;
