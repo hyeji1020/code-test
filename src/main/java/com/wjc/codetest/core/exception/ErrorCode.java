ade78ff645f8cbb60adfca2080c13120aafad2ef
hyeji <hyeji6742@naver.com>
2025-11-02 23:32:59 +0900

    refactor: ErrorCode 수정

--- src/main/java/com/wjc/codetest/core/exception/ErrorCode.java
+++ src/main/java/com/wjc/codetest/core/exception/ErrorCode.java
@@ -8,9 +8,8 @@ import lombok.Getter;
 public enum ErrorCode {
 
     PRODUCT_NOT_FOUND("해당 상품을 찾을 수 없습니다."),
-    BAD_REQUEST("잘못된 요청입니다.")
     ;
 
-    private final String message;
+    private final String defaultMessage;
 
 }
